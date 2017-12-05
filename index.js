919fd562fd6e65825bb95451536bcf172b0b425f
Lin Wei <linwei0201@126.com>
2017-12-05 18:04:55 +0800

    add

--- index.js
+++ index.js
@@ -74,13 +74,33 @@ app.post('/login', function(req, res, next){
     }
 });
 
+
+// 退出登录
+app.get('/logout', function(req, res, next){
+    // 备注：这里用的 session-file-store 在destroy 方法里，并没有销毁cookie
+    // 所以客户端的 cookie 还是存在，导致的问题 --> 退出登陆后，服务端检测到cookie
+    // 然后去查找对应的 session 文件，报错
+    // session-file-store 本身的bug
+
+    req.session.destroy(function(err) {
+        if(err){
+            res.json({ret_code: 2, ret_msg: '退出登录失败'});
+            return;
+        }
+
+        // req.session.loginUser = null;
+        res.clearCookie(identityKey);
+        res.redirect('/');
+    });
+});
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  // console.log('a user connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
 });
